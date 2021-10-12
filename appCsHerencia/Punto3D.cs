9ec3fb14d0773f946cf4cf2e4b9822612d52ba27
Zurita <af3392976@gmail.com>
2021-10-12 10:16:41 -0500

    Add project files.

---
+++ appCsHerencia/Punto3D.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace appHerencia
+{
+    class Punto3D : Punto2D
+    {
+        private double z;
+
+        public double Z
+        {
+            get { return z; }
+            set { z = value; }
+        }
+        private double distance;
+
+        public double Distance
+        {
+            get { return distance; }
+            set { distance = value; }
+        }
+
+        public Punto3D() : base()
+        {
+            z = 0;
+        }
+        public Punto3D(double x, double y, double z) : base(x, y)
+        {
+            this.z = z;
+            distance = Math.Sqrt(Math.Pow(x - y, 2));
+        }
+        public override string ToString()
+        {
+            return base.ToString() + " Z=" + z + "\nDistancia de X a Y= " + distance;
+        }
+    }
+}
